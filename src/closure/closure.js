17c80ce03e03dee94b19926563142e2f1525ce6d
creativoo <creativoo@gmail.com>
2023-02-22 10:26:43 -0700

    aprendiendo de closures y creando my money box

---
+++ src/closure/closure.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let username = 'Oscar';
+
+    function displayUserName() {
+        return `¡Hello ${username}!`;
+    }
+    return displayUserName;
+}
+
+const g = greeting();
+console.log(g);
+console.log(g());
